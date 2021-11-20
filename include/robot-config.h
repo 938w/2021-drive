0a3a1002cb81f75f57a1cee78d48a0606cda8b80
runchenwu <60835042+runchenwu@users.noreply.github.com>
2021-11-20 15:54:08 -0800

    update

--- include/robot-config.h
+++ include/robot-config.h
@@ -2,9 +2,18 @@ using namespace vex;
 
 extern brain Brain;
 
+// VEXcode devices
+extern motor FrontLeft;
+extern motor FrontRight;
+extern motor BackLeft;
+extern motor BackRight;
+extern motor MobileLift;
+extern motor Intake;
+extern controller Controller1;
+
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void);
+void  vexcodeInit( void );
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,15 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// FrontLeft            motor         1               
+// FrontRight           motor         2               
+// BackLeft             motor         3               
+// BackRight            motor         4               
+// MobileLift           motor         5               
+// Intake               motor         6               
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -32,7 +41,13 @@ competition Competition;
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
+  MobileLift.setVelocity(100,percent);
+  MobileLift.setMaxTorque(100,percent);
+ FrontLeft.setVelocity(100,percent);
+ FrontRight.setVelocity(100,percent);
+ BackLeft.setVelocity(100,percent);
+ BackRight.setVelocity(100,percent);
+ Intake.setVelocity(100,percent);
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
@@ -69,13 +84,40 @@ void usercontrol(void) {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
-
+    //Mobile Lift Coding
+ if (Controller1.ButtonL1.pressing()) {
+ MobileLift.spin(forward);
+ }
+ 
+ if (Controller1.ButtonL2.pressing()) {
+ MobileLift.spin(reverse);
+ }
+ 
+ if (!Controller1.ButtonL1.pressing() && !Controller1.ButtonL2.pressing()) {
+ MobileLift.stop();
+ }
+ 
+ if (!Controller1.ButtonR1.pressing()) {
+ Intake.spin(forward);
+ }
+ 
+ if (!Controller1.ButtonR2.pressing()) {
+ Intake.spin(reverse); 
+ }
+ 
+ if (!Controller1.ButtonR1.pressing() && !Controller1.ButtonR2.pressing()) {
+ Intake.stop();
+ }
+    FrontLeft.spin(vex::directionType::rev, Controller1.Axis3.position(), vex::velocityUnits::pct);
+    FrontRight.spin(vex::directionType::rev, Controller1.Axis2.position(), vex::velocityUnits::pct);
+    BackLeft.spin(vex::directionType::rev, Controller1.Axis3.position(), vex::velocityUnits::pct);
+    BackRight.spin(vex::directionType::rev, Controller1.Axis2.position(), vex::velocityUnits::pct);
     // ........................................................................
     // Insert user code here. This is where you use the joystick values to
     // update your motors, etc.
     // ........................................................................
 
-    wait(20, msec); // Sleep the task for a short amount of time to
+    wait(10, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
 }
