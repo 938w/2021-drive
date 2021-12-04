3a5d41446c6d30bd6e43dc10a226cbb7b23d269f
Sidhu938W <94768561+Sidhu938W@users.noreply.github.com>
2021-12-03 19:36:33 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -420,7 +420,7 @@ void autonomous(void) {
   BackRight.setVelocity(30, percent);
   BackLeft.setVelocity(30, percent);
   //Deploy front lift
-  FrontLift("down", 0.5.5); 
+  FrontLift("down", 5.5); 
   //Move forward
   drivefunctions("fwd", 0.3);
   /*
